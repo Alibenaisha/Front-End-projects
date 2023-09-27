7b1850c7adbd4d64e2ed407b3a521680db4bd25b
Alibenaisha <alibenaisha3@gmail.com>
2023-09-27 22:27:33 +0200

    add Random Element From Sequence

---
+++ Random Element From Sequence/main.js
@@ -0,0 +1,10 @@
+let box = ["ali","ahmad","mohamed"];
+
+console.log(box);
+console.log( box.length);
+console.log( Math.random()*3);
+
+console.log( Math.floor(Math.random()*box.length));
+console.log( Math.trunc(Math.random()*box.length));
+
+console.log(box[Math.floor(Math.random()*box.length)]);
