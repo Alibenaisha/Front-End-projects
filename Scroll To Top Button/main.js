453eb474351c74164396ba22529c9fdd07f3d0a0
Alibenaisha <alibenaisha3@gmail.com>
2023-09-30 16:31:12 +0200

    add Scroll To Top Button

---
+++ Scroll To Top Button/main.js
@@ -0,0 +1,15 @@
+
+
+let span = document.querySelector(".up");
+// console.log(span);
+window.onscroll = ()=>{
+console.log(this.scrollY);
+this.scrollY>=1000 ? span.classList.add("show"):span.classList.remove("show");
+
+}
+span.onclick= ()=>{
+    window.scrollTo({
+        top : 0,
+        behavior:"smooth"
+    })
+}
