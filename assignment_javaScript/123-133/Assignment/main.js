7c2314c667fdc0f672087a5935eee796185d2865
Alibenaisha <alibenaisha3@gmail.com>
2023-10-03 13:10:24 +0200

    Assignment 123-133 Q1

---
+++ assignment_javaScript/123-133/Assignment/main.js
@@ -0,0 +1,15 @@
+console.log(`========Q1=======`);
+
+
+let q1= new Set().add(10);
+// q1.add(10)
+q1.add(20)
+q1.add(2)               // Needed Output
+console.log(q1);        // Set(3) {10, 20, 2}
+console.log([...q1][2]);// 2
+
+
+
+
+
+console.log(`========Q2=======`);
