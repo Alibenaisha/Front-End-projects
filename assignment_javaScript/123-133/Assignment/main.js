fa619d3c832a3560f5f9e4136d9b6636f1b3f932
Alibenaisha <alibenaisha3@gmail.com>
2023-10-04 23:57:56 +0200

    add

--- assignment_javaScript/123-133/Assignment/main.js
+++ assignment_javaScript/123-133/Assignment/main.js
@@ -7,9 +7,13 @@ q1.add(20)
 q1.add(2)               // Needed Output
 console.log(q1);        // Set(3) {10, 20, 2}
 console.log([...q1][2]);// 2
-
-
-
-
-
 console.log(`========Q2=======`);
+
+let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+
+let q2 = new Set(myFriends);
+console.log([...q2].sort());
+// Needed Output
+//(4) ['Ahmed', 'Mahmoud', 'Osama', 'Sayed']
+console.log(`========Q3=======`);
+
